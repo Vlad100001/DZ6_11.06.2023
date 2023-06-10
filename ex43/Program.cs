b83cc96a18d8da1570b5ad1dc37c3787c6286f66
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-06-11 00:02:00 +0300

    Update Program.cs

--- ex43/Program.cs
+++ ex43/Program.cs
@@ -19,7 +19,7 @@ else
     
 x=(b2-b1)/(k1-k2);
 y=(k1*(b2-b1))/(k1-k2)+b1;
-x=Math.Round(x, 3);
-y=Math.Round(y, 3);
+x=Math.Round(x, 4);
+y=Math.Round(y, 4);
 Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 }
